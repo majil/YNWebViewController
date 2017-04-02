15558be6128a1fe9f684af714b083c7381023ae1
tommy <xu_yunan@163.com>
2017-04-02 19:29:57 +0800

    update

--- YNWebViewController/YNWebViewController.swift
+++ YNWebViewController/YNWebViewController.swift
@@ -155,15 +155,5 @@ public class YNWebViewController: UIViewController, UIWebViewDelegate {
         self.webView.delegate = nil
         UIApplication.shared.isNetworkActivityIndicatorVisible = false
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
